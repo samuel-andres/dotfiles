53250c9e7680b7e8b9a94aa79056c3c92ac774c9
Samuel Andrés <samuel5848@gmail.com>
2024-05-20 19:46:30 -0300

    KEKW

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,12 +6,15 @@ local M = {}
 
 M.ui = {
     theme = "monekai",
-    transparency = true,
+    transparency = false,
 
     hl_override = {
         Comment = { italic = true },
         ["@comment"] = { italic = true },
     },
+    statusline = {
+        separator_style = "block",
+    }
 }
 
 return M
