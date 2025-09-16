568efc9ae93a169d6b8fc74380db3e82983d338f
Samuel Andrés <samuel5848@proton.me>
2025-09-16 00:26:24 -0300

    update vim config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,40 +1,18 @@
 return {
   'neovim/nvim-lspconfig',
-  event = { 'BufReadPost', 'BufNewFile' },
-  cmd = { 'LspInfo', 'LspInstall', 'LspUninstall' },
   dependencies = {
-    'williamboman/mason.nvim',
-    'williamboman/mason-lspconfig.nvim',
+    { 'mason-org/mason.nvim', opts = {} },
+    'mason-org/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     'saghen/blink.cmp',
     { 'kevinhwang91/nvim-ufo', dependencies = { 'kevinhwang91/promise-async' } },
   },
   config = function()
-    local servers = {
-      pyright = {
-        capabilities = { textDocument = { publishDiagnostics = { tagSupport = { valueSet = { 2 } } } } },
-        settings = { python = { analysis = { typeCheckingMode = 'off' } } },
-      },
-      lua_ls = { settings = { Lua = { completion = { callSnippet = 'Replace' } } } },
-    }
-
-    require('mason').setup()
-
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, { 'stylua', 'html-lsp', 'css-lsp', 'ts_ls', 'dockerls', 'angular-language-server' })
-
-    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-    require('mason-lspconfig').setup {
-      ensure_installed = {},
-      automatic_enable = true,
-      automatic_installation = false,
-      handlers = {
-        function(server_name)
-          local server = servers[server_name] or {}
-          require('lspconfig')[server_name].setup(server)
-        end,
-      },
-    }
+    vim.lsp.config('pyright', {
+      settings = { python = { analysis = { typeCheckingMode = 'off' } } },
+      capabilities = { textDocument = { publishDiagnostics = { tagSupport = { valueSet = { 2 } } } } },
+    })
+    require('mason-lspconfig').setup { ensure_installed = { 'pyright', 'lua_ls', 'html', 'cssls', 'ts_ls', 'dockerls', 'angularls' } }
     require('ufo').setup()
   end,
 }
